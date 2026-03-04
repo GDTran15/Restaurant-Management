1d09de91261aaec188a26fa6a8a911abfa762876
GDTran15 <trangiaiduong1512@gmail.com>
2026-03-04 12:03:41 +1100

    fix inputComponent

--- frontend/src/component/InputField.jsx
+++ frontend/src/component/InputField.jsx
@@ -4,7 +4,7 @@ export default function InputField({label,placeholder, inputType,value , setValu
             <label className=" block text-base mb-2">
                 {label}
             </label>
-            <input type={inputType} value={value} placeholder={placeholder}
+            <input type={inputType}  value={value} placeholder={placeholder}
              className={`border p-1.5 rounded w-full bg-input-bg focus:outline-none focus:ring-0 focus:border-grey-600 mb-2 `}
              onChange={(e) => setValue(e.target.value)  }
              />
