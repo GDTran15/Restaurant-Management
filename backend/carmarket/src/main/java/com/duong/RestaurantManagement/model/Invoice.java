0ab56610d8969c5a4dd37d02ced817e1255d1663
GDTran15 <trangiaiduong1512@gmail.com>
2026-04-13 17:14:39 +1000

    git add builder library into Invoice entity

--- backend/carmarket/src/main/java/com/duong/RestaurantManagement/model/Invoice.java
+++ backend/carmarket/src/main/java/com/duong/RestaurantManagement/model/Invoice.java
@@ -2,6 +2,7 @@ package com.duong.RestaurantManagement.model;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -13,6 +14,7 @@ import java.util.List;
 @Data
 @NoArgsConstructor
 @Table(name = "invoices")
+@Builder
 public class Invoice {
 
     @Id
