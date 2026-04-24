0c9da2a1c4607535b6ccfc9d8fe2839b59289f8d
GDTran15 <trangiaiduong1512@gmail.com>
2026-04-24 16:29:46 +1000

    create Invoice Repo

--- backend/carmarket/src/main/java/com/duong/RestaurantManagement/repo/InvoiceRepo.java
+++ backend/carmarket/src/main/java/com/duong/RestaurantManagement/repo/InvoiceRepo.java
@@ -1,7 +1,10 @@
 package com.duong.RestaurantManagement.repo;
 
 import com.duong.RestaurantManagement.model.Invoice;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface InvoiceRepo extends JpaRepository<Invoice, Long> {
+
+    boolean existsById(@NotNull Long invoiceId);
 }
